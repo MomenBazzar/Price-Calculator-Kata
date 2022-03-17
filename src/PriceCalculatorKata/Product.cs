4db6a716026dba86cc89fa72c9f27c1e0be49fa0
Momen Bazzar <momen.b2001@gmail.com>
2022-03-17 21:54:30 +0200

    first commit

---
+++ src/PriceCalculatorKata/Product.cs
@@ -0,0 +1,9 @@
+namespace PriceCalculatorKata
+{
+    public class Product
+    {
+        public string? Name { get; set; }
+        public int UPC { get; set; }
+        public double Price { get; set; }
+    }
+}
